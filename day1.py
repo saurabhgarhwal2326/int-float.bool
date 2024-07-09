74734aadc50882352a82b56b1cbd8985e7cd3a76
saurabh garhwal <saurabhgarhwal353@gmail.com>
2024-07-09 23:34:21 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,87 @@
+print('hello world')
+var = 10.67    #float 
+var2 = 77  #integer
+print(var)
+print(type(var))
+
+
+
+var1 = True 
+var2 = False  
+print(var2) 
+print(type(var1))
+print(type(var2))
+
+var = 18 
+print(var)
+print(type(var))
+
+var = 12.33
+print(var)
+print(type(var))
+
+
+var1 = True
+var2 = False
+print(var1)
+print(var2)
+print(type(var1))
+print(type(var2))
+
+print("hello word")
+var = 20.99  #float 
+var2 = 30  # intiger
+print(var)
+print(type(var))
+
+
+print("hello word")
+var = 3.54
+var2 = 342
+print(var2)
+print(type(var2))
+
+#bool
+var1= True
+var2= False
+print(var2)
+print(type(var1))
+print(type(var2))
+
+#intiger
+var =23
+print(var)
+print(type(var))
+
+#float
+var = 23.443
+print(var)
+print(type(var))
+
+#true false 
+#bool 
+
+var1 = True
+var2 = False
+print(var1)
+print(var2)
+print(type(var1))
+print(type(var2))
+
+
+# practics 
+
+var = 23.55
+var2 = 214
+print(var)
+print(var2)
+print(type(var))
+print(type(var2))
+
+
+var1 = True 
+var2  = False
+print(var1)
+print(var2)
+print(type(var1))
+print(type(var2))
